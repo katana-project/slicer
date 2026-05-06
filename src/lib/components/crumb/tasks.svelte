75f0c8f46ae3a1ba7c6f2936ee82f7187fc7b9f3
Matouš Kučera <mk@kcra.me>
2026-05-06 15:08:59 +0200

    chore: replace task loader circle with progress bar
    Closes #75.

--- src/lib/components/crumb/tasks.svelte
+++ src/lib/components/crumb/tasks.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
     import { t } from "$lib/i18n";
     import type { Task } from "$lib/task";
-    import { LoaderCircle } from "@lucide/svelte";
     import { Popover, PopoverContent, PopoverTrigger } from "$lib/components/ui/popover";
     import TaskComponent from "./task.svelte";
+    import { Progress } from "$lib/components/ui/progress";
 
     interface Props {
         tasks: Task[];
     }
 
     let { tasks }: Props = $props();
-    let mainName = $derived(tasks[0]!.name);
+    let mainTask = $derived(tasks[0]!);
+    let mainName = $derived(mainTask.name);
+    let mainProgress = $derived(mainTask.progress);
 </script>
 
 {#if tasks.length > 0}
     <Popover>
-        <PopoverTrigger class="flex flex-row items-center">
-            <LoaderCircle size={12} class="text-primary mr-1 animate-spin" />
+        <PopoverTrigger class="flex flex-row items-center gap-2">
             <span>
                 {$t(tasks.length > 1 ? "crumb.task.multiple" : "crumb.task", $t($mainName), tasks.length - 1)}
             </span>
+            <Progress value={$mainProgress ?? 100} indeterminate={!$mainProgress} class="h-1.5 w-32" />
         </PopoverTrigger>
         <PopoverContent class="z-30 flex max-h-64 w-72 flex-col p-0" side="top" align="end" sideOffset={14}>
             <p class="border-b-border border-b px-4 py-2 text-center text-sm font-medium">
